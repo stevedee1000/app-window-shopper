82b0059b55d8cedf3250438ae1c92e9957a076df
stevedee1000 <stevedee1000@sbcglobal.net>
2017-08-15 23:40:20 -0700

    Programmed currency text field

--- window-shopper/window-shopper/Controller/MainVC.swift
+++ window-shopper/window-shopper/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  window-shopper
 //
 //  Created by Stephen Desterhaft on 8/15/17.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,25 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Stephen Desterhaft on 8/15/17.
+//  Copyright © 2017 Stephen Desterhaft. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2461740154)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+}
